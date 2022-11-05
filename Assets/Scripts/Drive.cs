d8cc89c6d63ed8310da2b2748fc1b21f44cb752f
DiogoGananca <a47508@alunos.ipb.pt>
2022-11-05 17:26:40 +0000

    Calculating Trajectories
    Último tutorial.

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -10,19 +10,21 @@ public class Drive : MonoBehaviour {
     public GameObject bulletObj;
 
     void Update() {
-        
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
         float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 
-        
+        // Make it move 10 meters per second instead of 10 meters per frame...
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
 
-        
+        // Move translation along the object's z-axis
         transform.Translate(0, 0, translation);
-        
+        // transform.Translate(0, 0, speed * Time.deltaTime);
 
-        
+        // Rotate around our y-axis
         transform.Rotate(0, rotation, 0);
 
         if (Input.GetKey(KeyCode.T)) {
